7d4803b3d5668021c470628470ade89dee52849a
ramees <rameestp001.com>
2024-04-08 17:07:24 +0530

    updation in backend baseURL

--- src/api/worker/workerInstance.js
+++ src/api/worker/workerInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:9170",
-  baseURL: 'http://184.73.25.154'
+  baseURL: 'http://184.73.25.154/api/'
 });
 
 axiosInstance.interceptors.request.use(
